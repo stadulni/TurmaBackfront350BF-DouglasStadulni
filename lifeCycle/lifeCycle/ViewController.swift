a5e94e4f3649af2c7e3d1e9c20e73dd25507d1a9
Douglas Stadulni <stadulnidouglas@gmail.com>
2023-04-04 19:54:56 -0300

    lifeCycle criado

---
+++ lifeCycle/lifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifeCycle
+//
+//  Created by Douglas Stadulni on 28/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
